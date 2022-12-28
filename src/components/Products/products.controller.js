e71abc05ea9e5f9375792ca0c286151b48b069d2
Diego Pisfil Rios <fdiego.prios@gmail.com>
2022-12-27 20:06:57 -0500

    refactoring patch point

--- src/components/Products/products.controller.js
+++ src/components/Products/products.controller.js
@@ -24,14 +24,14 @@ product.get = async (req, res) => {
 
 product.post = async (req, res) => {
     if (!req.body) return res.status(401).send({ message: '=body: !' })
-    const file_uri = req.body.file
+    const file_b64 = req.body.file
     delete req.body.file
 
     let body = req.body;
 
     body.stock = body._stock
 
-    cloudinary.uploader.upload(file_uri, {
+    cloudinary.uploader.upload(file_b64, {
         public_id: body.cod,
         folder: 'dans',
         unique_filename: true
@@ -66,10 +66,11 @@ product.removeOne = async (req, res) => {
     await Product.findOneAndDelete({ cod: id }, (err, doc) => {
         if (err) return res.status(500).send({ msg: err })
 
-        console.log(doc)
+        console.log('-> DELETE OK')
 
         cloudinary.uploader.destroy('dans/' + doc.cod, { invalidate: true, resource_type: 'image' })
             .then((result) => {
+                console.log('-> ... CLD')
                 console.log(result)
                 return res.status(200).json({ data: doc })
             })
@@ -80,10 +81,44 @@ product.patch = async (req, res) => {
     const id = req.params.id;
     const options = { new: true };
 
-    await Product.findByIdAndUpdate(id, req.body, options, (err, doc) => {
-        if (err) return res.status(500).send({ msg: err })
-        return res.status(200).json({ data: doc })
-    })
+    let data = req.body;
+
+    if (data.file === '') {
+        delete data.file
+
+        await Product.findByIdAndUpdate({ cod: id }, data, options, (err, doc) => {
+            if (err) return res.status(500).send({ msg: err })
+            return res.status(200).json({ data: doc })
+        })
+    } else {
+        cloudinary.uploader.destroy('dans/' + data.cod, { invalidate: true, resource_type: 'image' }) // delete old image
+            .then((result) => {
+                console.log('-> ... CLD')
+                console.log(result)
+
+                cloudinary.uploader.upload(data.file, { // up new image
+                    public_id: data.cod,
+                    folder: 'dans',
+                    unique_filename: true
+                }, async function (err, result) {
+
+                    if (err) {
+                        res.status(500).send({ message: 'Conflicto al subir la imagen' })
+                        console.log(err);
+                        return
+                    }
+
+                    data.img_uri = result.secure_url;
+
+                    await Product.findByIdAndUpdate({ cod: id }, data, options, (err, doc) => {
+                        if (err) return res.status(500).send({ msg: err })
+                        return res.status(200).json({ data: doc })
+                    })
+                })
+            })
+    }
+
+
 }
 
 module.exports = product;
