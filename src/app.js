5224a659c56b714e24df0534d7c9bd222bc7bc0b
DiegoPisfilRios <77468729+DiegoPisfilRios@users.noreply.github.com>
2023-03-10 17:50:56 +0000

    views

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { engine } = require('express-handlebars');
 var cors = require('cors')
 const morgan = require('morgan')
 
@@ -8,7 +9,9 @@ app.use(cors());
 app.use(morgan('dev'))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
-
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 app.use(express.static('public'));
 
 
