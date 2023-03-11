3a0b0bbab3c7804a9798298d6141f6fdcb6a2be2
DiegoPisfilRios <77468729+DiegoPisfilRios@users.noreply.github.com>
2023-03-11 13:18:40 +0000

    .hbs

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ var hbs = exphbs.create({
     extname: ".hbs",
 });
 
+app.set('views', join(__dirname, 'views'));
+app.use(express.static(join(__dirname, 'public')));
 app.use(cors());
 app.use(morgan('dev'))
 app.use(express.json({ limit: '50mb' }))
@@ -20,8 +22,6 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
-app.set('views', join(__dirname, 'views'));
-app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
     res.render('home');
