4498bdfbd7ed239bf61ae0350ecc7fc241b31bb1
DiegoPisfilRios <77468729+DiegoPisfilRios@users.noreply.github.com>
2023-03-11 13:28:48 +0000

    set

--- src/app.js
+++ src/app.js
@@ -2,9 +2,19 @@ const express = require('express')
 var exphbs = require('express-handlebars');
 var cors = require('cors')
 const morgan = require('morgan');
-const { join } = require('path');
+const { join, dirname } = require('path');
+const { fileURLToPath } = require('url');
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', join(__dirname, 'views')); 
+app.use(morgan('dev'))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
+app.use(cors());
+app.use(express.json({ limit: '50mb' }))
 
 var hbs = exphbs.create({
     defaultLayout: "main",
@@ -13,22 +23,14 @@ var hbs = exphbs.create({
     extname: ".hbs",
 });
 
-app.set('views', join(__dirname, 'views'));
-app.use(express.static(join(__dirname, 'public')));
-app.use(cors());
-app.use(morgan('dev'))
-app.use(express.json({ limit: '50mb' }))
-app.use(express.urlencoded({ limit: '50mb', extended: true }))
-
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
+
+// static files
+app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
     res.render('home');
 });
-
-
-
-app.set('port', process.env.PORT || 4000)
 
 module.exports = app
