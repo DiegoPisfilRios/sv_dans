a048b6c1d746eb7957abbfbd9b714cd4db22046c
DiegoPisfilRios <77468729+DiegoPisfilRios@users.noreply.github.com>
2023-03-11 02:59:02 +0000

    handlebars

--- src/app.js
+++ src/app.js
@@ -9,9 +9,10 @@ app.use(cors());
 app.use(morgan('dev'))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
+
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+// app.set('views', './views');
 app.use(express.static('public'));
 
 
